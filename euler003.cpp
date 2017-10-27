5ca20b45425c9686227eb54a217c1d2e46d23f29
danpryer <danpryer1@gmail.com>
2017-10-27 16:28:35 +0100

    potential working solution to problem 3

--- euler003.cpp
+++ euler003.cpp
@@ -4,9 +4,20 @@
 #include <iostream>
 
 int main(){
-  int theprime = 600851475143;
-
-
-
+  double theprime = 600851475143;
+  int primefinder = (theprime + 1) / 2; //start from the half way point
+  bool primefound = false;
+  do{
+    for (primefinder > 2; primefinder = primefinder - 1){
+      //check to see if primefinder is a prime number
+      bool isprime = true;
+      int i;
+      for (i = primefinder / 2; i >2; i = i - 1){
+        if (primefinder % i == 0)
+          isprime = false;
+          break;
+      }
+    }
+  } while (primefound == false);
   return 0;
 }
