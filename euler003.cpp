399fa1f591dd42c4ea910933d7953dda7fb43253
danpryer <danpryer1@gmail.com>
2017-10-28 00:33:29 +0100

    fast working solution to problem 3

--- euler003.cpp
+++ euler003.cpp
@@ -2,22 +2,21 @@
 // Find the largest prime factor of the number 600851475143
 
 #include <iostream>
+#include <math.h>
 
 int main(){
-  double theprime = 600851475143;
-  int primefinder = (theprime + 1) / 2; //start from the half way point
+  long N, maxprime, i, x;
+  std::cout << "pls give integer" << std::endl;
+  std::cin >> N;
+  x = N;
   bool primefound = false;
-  do{
-    for (primefinder > 2; primefinder = primefinder - 1){
-      //check to see if primefinder is a prime number
-      bool isprime = true;
-      int i;
-      for (i = primefinder / 2; i >2; i = i - 1){
-        if (primefinder % i == 0)
-          isprime = false;
-          break;
-      }
+  for (i=2; i<sqrt(N);i=i+1){
+    if (N % i == 0){
+      std::cout << N << " is divisible by " << i << std::endl;
+      N = N/i;
     }
-  } while (primefound == false);
+  }
+  std::cout << "The largest prime factor of " << x;
+  std::cout << " is " << N << std::endl;
   return 0;
 }
