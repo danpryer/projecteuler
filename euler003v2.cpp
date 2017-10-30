ef51ff7aaf8cbcb7fa15723ea9ff0d3fbc9a4178
danpryer <danpryer1@gmail.com>
2017-10-30 12:06:30 +0000

    written a v2 to problem 3 as the general solution for any number (slow)

---
+++ euler003v2.cpp
@@ -0,0 +1,39 @@
+// 2nd attempt at euler 3
+
+#include <iostream>
+#include <math.h>
+
+int checkprime(long x) { //function which checks if input number is prime
+  long i;
+  bool isPrime = true;
+
+  for(i = 2; i <= x / 2; i=i+1) {
+    if(x % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  return isPrime;
+}
+
+int main(){
+  long N = 600851475143;
+  long j;
+  bool primefound = false;
+  if (checkprime(N) == true) {
+    primefound = true;
+    std::cout << "The original number entered, " << N << ", is a prime!" << std::endl;
+  }
+  do {
+    for (j = (N+1)/2; j > 1; j = j - 1) {
+      if (N % j == 0) { // check if j a factor of N
+        if (checkprime(j) == true) { // check if j a prime
+          primefound = true;
+          std::cout << "The largest prime factor is " << j << std::endl;
+          break;
+        } //end if
+      } //end if
+    } // end for
+  } while (primefound ==false); // end do
+  return 0;
+}
