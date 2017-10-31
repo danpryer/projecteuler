33afd16b10e49a11580e5bac4efece040b2b0559
danpryer <danpryer1@gmail.com>
2017-10-31 16:12:32 +0000

    correct working solution to problem 4

--- euler004.cpp
+++ euler004.cpp
@@ -17,17 +17,18 @@ int reverse(int i){ // function to reverse an input integer i
 }
 
 int main() {
-  int x, y, prod;
-  for (x = 999; x > 0; x = x - 1) { //start from largest x and count down
-    for (y = 999; y > 0; y = y - 1) { //start from largest x and count down
+  int x, y, prod, palindrome;
+  for (x = 999; x > 99; x = x - 1) { //start from largest x and count down
+    for (y = 999; y > 99; y = y - 1) { //start from largest x and count down
       prod = x * y;
-      std::cout << x << " * " << y << " = " << prod << std::endl; // tracer
-      if (prod == reverse(prod)) {
-        std::cout << "The largest palindromic number made from the product ";
-        std::cout << "of two 4-digit numbers is " << prod << std::endl;
-        y = x = 1;
+      if (prod == reverse(prod)) { //check if product is palindrome
+        if (prod > palindrome) { // is new palindrome larger?
+          palindrome = prod; //replace previous largest with new largest
+        } //end if
       } //end if
     } // end y for
   } // end x for
+  std::cout << "The largest palindromic number made from the product ";
+  std::cout << "of two 3-digit numbers is " << palindrome << std::endl;
   return 0;
 }
