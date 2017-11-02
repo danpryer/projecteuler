32fab0f7c5e35494211546f33854800460563d2c
danpryer <danpryer1@gmail.com>
2017-11-02 16:52:27 +0000

    initial code for problem

---
+++ euler012.cpp
@@ -0,0 +1,19 @@
+// Project Euler 12
+// What is the value of the first triangle number to have over five hundred divisors?
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <cstdlib>
+
+std::vector<int> factors;
+
+int main() {
+  long N, triangle, counter;
+  //while (std::count(factors.begin(), factors.end()) <= 500) { //need length not count
+  //WINS.push_back (rollnumber); //add to vector
+  //}
+
+
+  return 0;
+}
